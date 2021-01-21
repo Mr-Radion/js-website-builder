8080f922e46b243a46563ce8ab47af1487255b93
Mr.Radion <mr.rodion_oa@mail.ru>
2021-01-22 01:04:26 +0300

    Adding the options property to the model

--- src/model.js
+++ src/model.js
@@ -1,10 +1,10 @@
-import images from './assets/image.png';
+import image from './assets/unnamed.jpg';
 
 // Тут в каждом объекте смысловой блок html, который можно добавлять
 // Поменять на коллекцию Map()
 export const model = [
-  { type: 'title', value: 'content' },
+  { type: 'title', value: 'content', options: { tag: 'h2' } },
   { type: 'text', value: 'Ex culpa occaecat dolore minim commodo eiusmod.' },
   { type: 'colums', value: ['11111111', '22222222', '33333333'] },
-  { type: 'images', value: images },
+  { type: 'images', value: image },
 ];
