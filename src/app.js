5c600eda15d9a86440d005a347297272c0d567a2
Mr.Radion <mr.rodion_oa@mail.ru>
2021-05-13 03:10:43 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ export class App {
   init() {
     const site = new Site('.content');
     site.render(this.model);
+    // вместо колбэка реализовать Event Emitter
     const updateCallback = (newBlock) => {
       this.model.push(newBlock);
       site.render(this.model);
